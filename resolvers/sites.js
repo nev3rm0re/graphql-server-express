35f6c0178f7b7d0e4864adc860c876f9e93c046c
German Rumm <german.rumm@gmail.com>
2021-11-23 12:34:35 +0000

    Query database for migration tables

--- resolvers/sites.js
+++ resolvers/sites.js
@@ -18,24 +18,37 @@ module.exports.typeDefs = `
     }
 `;
 
-const getInfoForSite = async (sitename) => {
+const getInfoForSite = async (sitename, getConnection) => {
   const fullpath = process.env.SITES_CONFIG_FOLDER + '/.env.' + sitename;
   const contents = await readFile(fullpath);
   const vars = dotenv.parse(contents);
+
+  const targetDbName =
+    vars['TARGET_DATABASE_NAME'] || vars['MIGRATION_SITENAME'] + '_target';
+
+  const conn = await getConnection(targetDbName);
+  const tables = await conn.query('SHOW TABLES LIKE "%_migration"');
+
   return {
     env: sitename,
     sitename: vars['MIGRATION_SITENAME'],
+    migrated: tables.length > 0,
   };
 };
 
 module.exports.resolvers = {
   Query: {
-    sites: async () => {
+    sites: async (_, args, context) => {
       const files = fs.readdirSync(process.env.SITES_CONFIG_FOLDER);
       const siteNames = files.reduce((carry, envFile) => {
         const matchSitename = envFile.match(/\.env\.(?<sitename>[a-z0-9_-]*)/);
         if (matchSitename) {
-          carry.push(getInfoForSite(matchSitename.groups['sitename']));
+          carry.push(
+            getInfoForSite(
+              matchSitename.groups['sitename'],
+              context.connectionManager,
+            ),
+          );
         }
 
         return carry;
