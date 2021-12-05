5210edfc4a9e64387ee0391cd55c28205465b05f
German Rumm <german.rumm@gmail.com>
2021-12-05 12:50:16 +0000

    Return sites list when requested without `requireSource` parameter

--- __tests__/sitesList.spec.js
+++ __tests__/sitesList.spec.js
@@ -1,4 +1,5 @@
 require('isomorphic-fetch');
+const getConnection = require('../database.js');
 
 const queryServer = (query) => {
   return fetch('http://104.154.240.101/graphql', {
@@ -11,6 +12,13 @@ const queryServer = (query) => {
 };
 describe('Query sites', () => {
   jest.setTimeout(20000);
+  test('what the heck mysql2.query returns', async () => {
+    const conn = await getConnection('2008ownersclub.co.uk_target');
+    const [result] = await conn.query('SHOW TABLES LIKE "%_migration"');
+
+    const tables = result.map((row) => Object.values(row)[0]);
+    console.debug(tables);
+  });
   test('SITES query returns expected result', async () => {
     jest.setTimeout(10 * 1000);
     expect.assertions = 1;
